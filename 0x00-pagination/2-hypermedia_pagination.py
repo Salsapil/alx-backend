41d08df339c5543f2ead3dbe4ccc13b6d7c9fe35
Salsabil <salsabeelahmed323@gmail.com>
2024-08-17 19:29:08 +0300

    2. Hypermedia pagination - edit output type

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -4,7 +4,7 @@
 
 import csv
 import math
-from typing import List
+from typing import List, Dict
 
 
 def index_range(page, page_size):
@@ -49,8 +49,8 @@ class Server:
             return []
 
         return dataset[start:end]
-    
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """Returns a dictionary with pagination metadata"""
         data = self.get_page(page, page_size)
         total_items = len(self.dataset())
