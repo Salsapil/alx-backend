5915b0177435331e17607bb1ad8f00b245fb317d
Salsabil <salsabeelahmed323@gmail.com>
2024-08-22 01:05:12 +0300

    edit LIFO task

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -10,19 +10,23 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """Initialize the FIFOCache class"""
         super().__init__()
+        self.stack = []  # keep track of insertion order
 
     def put(self, key, item):
         """assign to the dictionary self.cache_data the item value"""
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                del self.cache_data[key]
+        if key is None or item is None:
+            return
 
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                last_key = next(reversed(self.cache_data))
-                print(f"DISCARD: {last_key}")
-                self.cache_data.pop(last_key)
+        if key in self.cache_data:
+            self.stack.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # Remove the most recently added item
+            last_key = self.stack.pop()
+            del self.cache_data[last_key]
+            print(f"DISCARD: {last_key}")
 
-            self.cache_data[key] = item
+        self.cache_data[key] = item
+        self.stack.append(key)
 
     def get(self, key):
         """Return the value in self.cache_data linked to key."""
